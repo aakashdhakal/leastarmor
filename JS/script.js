3b90fd2cc213f522386221ed6abf4fe7f7a41634
A.D Entertainment <53038685+aakashdhakal@users.noreply.github.com>
2022-02-20 19:52:38 +0545

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+	loader.style.display = "none";
+});
+
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
